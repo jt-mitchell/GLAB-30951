2110b67d282b00fcf2ea8192e404723724dc814d
Jeff Mitchell <jeffreytm@gmail.com>
2024-12-26 19:46:56 -0500

    first commit

---
+++ src/main/java/com/example/ExerciseOne_Rest/ExerciseOneRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.ExerciseOne_Rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = {"com.example.ExerciseOne_Rest"})
+public class ExerciseOneRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExerciseOneRestApplication.class, args);
+	}
+
+}
